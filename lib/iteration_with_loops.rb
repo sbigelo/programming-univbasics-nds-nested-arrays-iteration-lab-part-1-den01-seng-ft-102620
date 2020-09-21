b2ad51a2dfa9ddd772c19b92ce36f5159ccbf0f2
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-21 17:21:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,7 @@ while count < src.length do
  
   inner_count = 0
   while inner_count < src[count].length do
+    if src[count][inner_count].even?
     p src[count][inner_count]
     inner_count += 1
   end
